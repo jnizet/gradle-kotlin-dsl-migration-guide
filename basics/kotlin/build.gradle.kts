30cd3c37a8333012f979a83cd43d6ec1bac86353
jnizet <jb@ninja-squad.com>
2018-05-13 16:25:26 +0200

    custom configurations

--- basics/kotlin/build.gradle.kts
+++ basics/kotlin/build.gradle.kts
@@ -112,3 +112,22 @@ dependencies {
     }
     testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine")
 }
+
+val db by configurations.creating
+val integTestImplementation by configurations.creating {
+    extendsFrom(configurations["testImplementation"])
+}
+
+dependencies {
+    db("org.postgresql:postgresql")
+    integTestImplementation("com.ninja-squad:DbSetup:2.1.0")
+}
+
+// get the existing testRuntimeOnly configuration
+val testRuntimeOnly by configurations
+
+dependencies {
+    testRuntimeOnly("org.postgresql:postgresql")
+    "db"("org.postgresql:postgresql")
+    "integTestImplementation"("com.ninja-squad:DbSetup:2.1.0")
+}
