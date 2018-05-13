6972cd3196ebe9e38a86b0af15e58e9fa600284d
jnizet <jb@ninja-squad.com>
2018-05-13 15:47:15 +0200

    declare dependencies

--- basics/kotlin/build.gradle.kts
+++ basics/kotlin/build.gradle.kts
@@ -102,3 +102,13 @@ tasks {
         from("doc")
     }
 }
+
+dependencies {
+    implementation("org.springframework.boot:spring-boot-starter-web")
+    implementation("io.jsonwebtoken:jjwt:0.9.0")
+    runtimeOnly("org.postgresql:postgresql")
+    testImplementation("org.springframework.boot:spring-boot-starter-test") {
+        exclude(module = "junit")
+    }
+    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine")
+}
