137269e405da232753b52e66c06530e7116af85a
jnizet <jb@ninja-squad.com>
2018-05-13 11:00:30 +0200

    apply external plugins

--- basics/kotlin/build.gradle.kts
+++ basics/kotlin/build.gradle.kts
@@ -1,6 +1,20 @@
+buildscript {
+    dependencies {
+        classpath("org.springframework.boot:spring-boot-gradle-plugin:2.0.1.RELEASE")
+    }
+}
+
 plugins {
     java
     id("jacoco")
+    id("org.flywaydb.flyway") version "5.0.3"
 }
 
 plugins.apply("checkstyle")
+
+repositories {
+    mavenCentral()
+}
+
+plugins.apply("io.spring.dependency-management")
+
